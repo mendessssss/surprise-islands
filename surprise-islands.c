127cf69315c1049df794d01cb52be619c59692f7
mendessssss <pricillia.mendes-freire@laplateforme.io>
2024-11-05 16:52:35 +0100

    reorganisation

--- surprise-islands.c
+++ surprise-islands.c
@@ -2,80 +2,85 @@
 #include <stdlib.h>
 #include <dirent.h>
 #include <string.h>
-#include "fonctions.c"
+#include <unistd.h>
 #define BUF_SIZE 255
+#define SAVE_SIZE 255
+#define VOYAGE 0
 
-struct perso {
+typedef struct jackie {
     int pv;
-    int endurance;
-    int pièces;
-};
-
-struct armes {
-    char * arme;
+    int or;
+    int attack;
+    char* arme;
     int degats;
-};
+    int rhum;
+} t_jacquie;
 
-struct ile1 {
-    int pirates;
-    int trésor;
-};
+typedef struct ennemis {
+    int nb;
+    int pv;
+    int attack;
+} t_ennemis;
+
+typedef struct ile {
+    int or;
+    struct ennemis mad;
+} t_ile; 
+
+int game_over (struct jackie *f) {
+    if (f->rhum == 0) {
+        f->pv --;
+        printf("GAME OVER\n");
+        return 0;
+    }
+}
+
+int victoire (struct jackie *p) {
+    if (p->or == 1000) {
+        printf("Vous êtes le king des océans bravo, votre fortune s'élève à 1 000 pièces d'or hamdoullah");
+        return 0;
+    }
+}
+
+int déplacement (struct jackie *m, char * mvt) {
+    while (VOYAGE > 1) {
+    mvt ++;
+    m->rhum --;
+    continue;
+    }
+}
 
 int main () {
 
+    printf("\n");
     printf("                   |    |     |\n");
     printf("                  )_)  )☠️)  )_)\n");
-    printf("                 )___))___))___)\\\n");
-    printf("                )____)____)_____)\\\\\n");
-    printf("              _____|____|____|____\\\\\\\n");
-    printf("    \\______________________________\\__\\\n");
+    printf("                 )___))___))___)\n");
+    printf("                )____)____)_____)\n");
+    printf("              _____|____|____|____\n");
+    printf("    ________________________________\n");
     printf(" ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
 
-    struct perso boug;
-    boug.pv = 600;
-    boug.endurance = 500;
-    boug.pièces = 100;
+    char* mvt;
 
-    struct armes piu;
-    piu.arme = "machette";
-    piu.degats = 100;
+    t_jacquie perso;
+    perso.pv = 600;
+    perso.or = 500;
+    perso.attack = 100;
+    perso.arme = "machette";
+    perso.degats = 15;
+    perso.rhum = 946;
 
-    struct ile1 lieu;
-    lieu.pirates = 48;
-    lieu.trésor = 4;
+    t_ennemis ratepi;
+    ratepi.nb = 17;
+    ratepi.pv = 100;
+    ratepi.attack = 40;
 
-    fclose(fopen("save", "a"));
+    t_ile bourbon;
+    bourbon.or = 80;
+    struct ennemis *mad;
 
-    FILE* ile  =fopen("ile", "r+");
-
-    fprintf (ile, "Ile des baléares");
-    fprintf (ile, "Ile Honshū");
-    fprintf (ile, "Ile Quesako");
-    fprintf (ile, "Ile Coupter");
-   
-
-    fclose(ile);
-
-    fclose(fopen("ile", "a"));
-
-    FILE* save  =fopen("save", "r+");
-
-    fprintf (save, "Charger une partie");
-    fprintf (save, "Créer une nouvelle partie");
-    fprintf (save, "Sauvegarder la partie en cours");
-
-    fclose(save);
-
-    fclose (fopen("inventaire.save", "a"));
-
-    FILE* fd = fopen("inventaire.save", "r+"); 
-
-    fprintf(fd, "PV : %d\nEndurance : %d\nPièces : %d\n", boug.pv, boug.endurance, boug.pièces);
-    fprintf(fd, "Equipement : %s %d\n", piu.arme, piu.degats);
-    fprintf(fd, "La première île est occupée par %d pirates, il y a %d trésors et il est possible d'avoir %d loot\n", lieu.pirates, lieu.trésor);
-    
-    fclose(fd);
-
+ 
     struct dirent *dir;
     int count = 0;
     char buf[BUF_SIZE]; memset(buf, 0, BUF_SIZE);
@@ -90,9 +95,26 @@ int main () {
             if ( strcmp( dir->d_name, "." ) && strcmp( dir->d_name, ".." ) ) {
                 printf("%d) - %s\n", count + 1, dir->d_name); // count +1 pour éviter de faire apparaitre le "0" devant le premier choix
                 count++;
-                }
+            }
         }
         closedir(dd);
     }
+
+    fclose (fopen("inventaire.save", "a"));
+
+    FILE* fd = fopen("inventaire.save", "r+"); 
+
+    fprintf(fd, "PV : %d\nEndurance : %d\nPièces : %d\n", perso.pv, perso.or);
+    fprintf(fd, "Equipement : %s %d\n", perso.arme, perso.degats);
+    fprintf(fd, "La première île est occupée par %d pirates et il y a %d trésors\n", ratepi.nb, bourbon.or);
+    
+    fclose(fd);
+
+// Accès écriture et lecture SANS SUPPRESSION DE DONNEES
+    fclose (fopen("save", "a"));
+    FILE* fe = fopen("save","r+");
+    char save[SAVE_SIZE];memset(save,0,SAVE_SIZE);      //à modifier
+    printf("%s\n",save);                                    
+
     return 0;
 }
