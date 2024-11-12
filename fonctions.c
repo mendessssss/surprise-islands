74017586afe058ff1abe0be5cb4fb5cf18529f0c
mendessssss <pricillia.mendes-freire@laplateforme.io>
2024-11-12 14:55:55 +0100

    reorg

--- fonctions.c
+++ fonctions.c
@@ -45,6 +45,20 @@ if (fichier == NULL){
 fclose(fichier);
 }
 
+FILE* fd = fopen ("save", "r+");
+char buf[255];memset (buf, 0, 255);
+
+while (fgets(buf, sizeof(buf), fd) != NULL) {
+    if (strncmp (buf, "pv:", 3) == 0) {
+        char * token = strtok (buf, ":");
+        token = strtok (NULL, "\n");
+        if  (token != NULL) {
+        boug.pv = atoi(token);
+    }
+    }
+} 
+
+printf ("pv: %d \nrhum : %d\nor : %d\n", boug.pv, trucs.rhum, trucs.or);
 
 
 
