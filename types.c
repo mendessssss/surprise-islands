ee7f0d847f69f733c2c7932037efb33a2d76156a
mendessssss <pricillia.mendes-freire@laplateforme.io>
2024-11-15 10:46:31 +0100

    branch

---
+++ types.c
@@ -0,0 +1,32 @@
+#define BUF_SIZE 255
+
+
+typedef struct jacquie {
+    int pv;
+    int or;
+    int attack;
+    int armes;
+    int rhum;
+} t_jacquie;
+
+// Jackie m : PV : 100, Or : 100, Attack : 5, Armes : 10, Rhum : 100
+t_jacquie m = {100, 100, 5, 10, 100};
+
+typedef struct squelette {
+    int pv;
+    int attack;
+} t_squelette;
+
+typedef struct ennemis {
+    int nb;
+    int pv;
+    int attack;
+} t_ennemis;
+
+typedef struct ile {
+    int or;
+    struct squelette* Mechant1;
+    struct ennemis mad;
+} ile; 
+
+char* mvt;
